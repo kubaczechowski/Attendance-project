16464ddae6defe93982b966a6e9951668e4e32e6
kubaczechowski <jcobczechowski@gmail.com>
2021-02-26 09:44:35 +0100

    deleted redundant code

--- src/sample/gui/controller/Teacher/CalendarViewController.java
+++ src/sample/gui/controller/Teacher/CalendarViewController.java
@@ -2,33 +2,22 @@ package sample.gui.controller.Teacher;
 
 import com.calendarfx.model.*;
 import com.calendarfx.view.CalendarView;
-import com.calendarfx.view.DateControl;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.application.Platform;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.util.Callback;
 import sample.gui.util.AddClassesToCal;
-
-import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ResourceBundle;
+
 /**
  * @author kuba
  */
 public class CalendarViewController implements Initializable {
 
     private CalendarView calendarView = new CalendarView();
-   // private CoursesModel coursesModel = new CoursesModel();
     private AddClassesToCal addClassesToCal = new AddClassesToCal();
     Calendar classes = new Calendar("Classes");
     @FXML
@@ -39,40 +28,20 @@ public class CalendarViewController implements Initializable {
         loadCalendar();
         updateTime();
         runCal();
-        openingEditDist();
-        calendarView.setEntryDetailsCallback(param -> {
-            System.out.println("we got it");
-            OpenEditAttendance.openEditAttendance(param.getEntry());
+        setClickListener();
+    }
 
+    /**
+     * method handles event of clicking on the entry
+     * then new window is opened
+     */
+    private void setClickListener() {
+        calendarView.setEntryDetailsCallback(param -> {
+            EditAttendance.OpenEditAttendance.openEditAttendance(param.getEntry());
             return true;
         });
-
-
     }
 
-    private void openingEditDist() {
-      /* borderPane.setOnMousePressed(MousePressedEventHandler);
-       borderPane.setOnMousePressed(clicked);*/
-       // calendarView.getWeekPage().setOnMousePressed(clicked);
-
-    }
-/*
-    EventHandler<MouseEvent> MousePressedEventHandler =
-            new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    if(mouseEvent.getSource() instanceof Entry){
-                        //open edit attendance window
-                        System.out.println("we got it");
-                        OpenEditAttendance.openEditAttendance((Entry)
-                                mouseEvent.getSource());
-                    }
-                }
-            };
-
-    EventHandler<MouseEvent> clicked = mouseEvent -> {
-        System.out.println(mouseEvent.getSource().toString());
-    };*/
 
     private void loadCalendar() {
         classes.setStyle(Calendar.Style.STYLE5);
@@ -107,30 +76,8 @@ public class CalendarViewController implements Initializable {
 
     private void runCal() {
         calendarView.showWeekPage();
-       // anchorPane.getChildren()add(calendarView);
         borderPane.setCenter(calendarView);
     }
 
-    static class OpenEditAttendance{
-        public static void openEditAttendance(Entry<?> lecture ){
-            FXMLLoader loader = new FXMLLoader(OpenEditAttendance.class.
-                    getResource("/sample/gui/view/Teacher/editAttendance.fxml"));
-            Parent root = null;
-            try {
-                root = loader.load();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            EditAttendance editAttendance = loader.getController();
-            editAttendance.setEntry(lecture);
-            Stage stage = new Stage();
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setScene(new Scene(root));
-            stage.showAndWait();
-            //think about moving window functionality
-        }
-
-
-    }
 
 }
